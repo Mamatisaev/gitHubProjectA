1a0ff966ddc3647968114453815bbea939f6597d
Mamatisaev <umamatisaev@gmail.com>
2022-04-26 09:14:19 +0600

    second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,17 @@
 package com.company;
 
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println(method(1, 300));
+    }
+    public static int method(int beginning, int ending) {
+        int counter = 0;
+        for (; beginning < ending; beginning++) {
+            if (beginning % 3 == 0 && beginning % 4 == 0 && beginning % 5 == 0) {
+                counter += beginning;
+            }
+        }
+        return counter;
     }
 }
