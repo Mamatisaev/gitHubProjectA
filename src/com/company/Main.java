3848da8495700b0bffdb5a48f8f73fb1ff79c653
Mamatisaev <umamatisaev@gmail.com>
2022-04-27 13:47:16 +0600

    third commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,15 +3,15 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(method(1, 300));
+        System.out.println("Sum: " + method(1, 300));
     }
-    public static int method(int beginning, int ending) {
-        int counter = 0;
-        for (; beginning < ending; beginning++) {
-            if (beginning % 3 == 0 && beginning % 4 == 0 && beginning % 5 == 0) {
-                counter += beginning;
+    public static int method(int a, int b) {
+        int i = 0;
+        for (; a < b; a++) {
+            if (a % 3 == 0 && a % 4 == 0 && a % 5 == 0) {
+            i += a;
             }
         }
-        return counter;
+        return i;
     }
 }
